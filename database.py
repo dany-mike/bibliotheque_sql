37061e2368b667f9320f654c844fd3eb27ef7b03
Dany <danymike.joseph@gmail.com>
2022-11-17 16:43:26 +0100

    feature: fix database connection

--- database.py
+++ database.py
@@ -6,29 +6,35 @@ from config import config
 class Database:
     def __init__(self):
         self.conn = self.connect()
-        # self.cur = self.conn.cursor()
+        self.cur = self.conn.cursor()
 
     def connect(self):
+        """ Connect to the PostgreSQL database server """
         conn = None
         try:
+            # read connection parameters
             params = config()
 
+            # connect to the PostgreSQL server
             print('Connecting to the PostgreSQL database...')
             conn = psycopg2.connect(**params)
 
+            # create a cursor
             cur = conn.cursor()
+
+            # execute a statement
             print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+
+            # display the PostgreSQL database server version
             db_version = cur.fetchone()
             print(db_version)
-            print(cur.statusmessage)
+
+            # close the communication with the PostgreSQL
             cur.close()
             return conn
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-                print('Database connection closed.')
 
     def close_connection(self):
         self.db.close()
