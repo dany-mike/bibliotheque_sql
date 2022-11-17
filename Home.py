d0d42ee01a0c10d2b49824fadd571aa10dc02326
Dany <danymike.joseph@gmail.com>
2022-11-17 16:03:38 +0100

    feature: connected to database done

--- Home.py
+++ Home.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
+from database import Database
 
+db = Database()
+
+db.test()
 st.set_page_config(
     layout="wide",
 )
 
 a, b = st.columns([1, 4])
 
-a.title("Home.py")
+a.title("Accueil bibliotheque")
--- database.py
+++ database.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python
 import psycopg2
 
+class Database:
+    def __init__(self):
+        self.db = self.connect()
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
+    def connect(self):
+        conn = None
+        try:
+            print('Connecting to the PostgreSQL database...')
+            conn = psycopg2.connect(
+                host="localhost",
+                database="bibliotheque",
+                user="admin",
+                password="admin",
+                port=5433)
 
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
+            cur = conn.cursor()
+            print('PostgreSQL database version:')
 
-        conn = psycopg2.connect(
-            host="localhost",
-            database="bibliotheque",
-            user="admin",
-            password="admin",
-            port=5433)
+            db_version = cur.fetchone()
+            print(db_version)
+            cur.close()
+            return conn
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
 
-        # create a cursor
-        cur = conn.cursor()
+    def close_connection(self):
+        self.cur.close()
+        self.db.close()
 
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
+    def test():
+        print('test')
