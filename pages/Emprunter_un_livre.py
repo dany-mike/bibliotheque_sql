cfd55de945b0756cf9886503e2130410eefd2410
Dany <danymike.joseph@gmail.com>
2022-11-24 12:19:37 +0100

    feature: blacklist wip

--- pages/Emprunter_un_livre.py
+++ pages/Emprunter_un_livre.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 from database import Database
 from book import Book
-# from personne import Personne
 
 db = Database()
 
@@ -16,7 +15,6 @@ a.title("Emprunter un livre")
 
 if len(st.experimental_get_query_params()) > 0: 
     personne_id = int(st.experimental_get_query_params()["personne_id"][0])
-    # p = Personne(personne_id, db)
     book = Book(db)
     book.renderBorrowBookForm(personne_id, book.getAvailableBooks())
 else :
--- personne.py
+++ personne.py
@@ -1,12 +1,10 @@
-# from book import Book
 class Personne:
     def __init__(self, personne_id, db):
         self.id = personne_id
         self.db = db
-        self.isBlacklisted()
 
-    # def isBlacklisted(self):
-    #     book = Book(self.db)
+    def isBlacklisted(self):
+        print('isBlacklisted')
 
     def getCurrentUser(self):
         self.db.cur.execute(
@@ -17,10 +15,11 @@ class Personne:
         self.db.cur.execute(
             "SELECT * FROM role WHERE personne_id = %s", (self.id,))
         return self.db.cur.fetchone()[0]
-    
+
     def getPersonneIdByName(self, nom):
         self.db.cur.execute("SELECT * FROM personne WHERE nom = %s", (nom,))
         return self.db.cur.fetchone()[0]
 
     def updateUserLimite(self, updated_limite, personne_id):
-        self.db.cur.execute("UPDATE personne SET limite = %s WHERE id = %s;", (updated_limite, personne_id))
+        self.db.cur.execute(
+            "UPDATE personne SET limite = %s WHERE id = %s;", (updated_limite, personne_id))
