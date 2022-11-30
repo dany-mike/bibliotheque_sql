49527bdb036e8037f879ebcc91eb4fb598c26da2
Dany <danymike.joseph@gmail.com>
2022-11-30 18:56:39 +0100

    feat: list of users using view done

---
+++ pages/Liste_des_personnes.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+from database import Database
+from personne import Personne
+
+db = Database()
+
+st.set_page_config(
+    layout="wide",
+)
+
+a, b = st.columns([1, 4])
+
+a.title("Liste des personnes")
+
+if len(st.experimental_get_query_params()) > 0:
+    personne_id = int(st.experimental_get_query_params()["personne_id"][0])
+    p = Personne(personne_id, db)
+    personne = p.getCurrentUser()
+    if personne[5] == 'admin':
+        p.renderMembersList()
+        p.renderAdminsList()
+    else:
+        st.text(
+            "Vous devez être connecté en tant qu'admin pour voir la liste des personnes")
+else:
+    st.text('Vous devez être connecté pour ajouter un livre')
